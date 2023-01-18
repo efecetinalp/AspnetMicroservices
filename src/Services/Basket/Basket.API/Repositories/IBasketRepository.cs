a50fba9e6c086d1a771103afec8904b69544096f
efecetinalp <efecetinalp@gmail.com>
2023-01-18 21:09:49 +0300

    created basket microservice

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        //redis -> key:username value:basket
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
